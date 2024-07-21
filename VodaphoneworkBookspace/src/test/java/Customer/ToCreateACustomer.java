ea9dadf7accf18b9717816cfb8658b61620d5a42
arjune221105 <shakti221105@gmail.com>
2024-07-21 05:52:15 -0700

    Adding Cricketer

---
+++ VodaphoneworkBookspace/src/test/java/Customer/ToCreateACustomer.java
@@ -0,0 +1,15 @@
+package Customer;
+
+public class ToCreateACustomer {
+
+	public static void main(String[] args) {
+		System.out.println("We are Filling Customer details");
+		System.out.println("MS Dhoni");
+		System.out.println("Virat Kolhi");
+		System.out.println("Rohit Shrma");
+		System.out.println("Surya Kumar Yadav");
+		System.out.println("Are To Create Jio Customer");
+		
+	}
+
+}
