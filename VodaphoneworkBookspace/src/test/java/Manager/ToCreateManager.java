e99fc9c37319e7974dcd05e71cd1d17e84df699a
arjune221105 <shakti221105@gmail.com>
2024-07-20 21:32:02 -0700

    add Manager

---
+++ VodaphoneworkBookspace/src/test/java/Manager/ToCreateManager.java
@@ -0,0 +1,10 @@
+package Manager;
+
+public class ToCreateManager {
+
+	public static void main(String[] args) {
+		System.out.println("TO Creat  A manager");
+		System.out.println("All Manager Employee");
+	}
+
+}
